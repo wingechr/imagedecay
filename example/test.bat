bffb41ce2170e2cb13307e2749924639e174355f
Christian Winger <c@wingechr.de>
2017-08-11 23:07:35 +0200

    scaling

--- example/test.bat
+++ example/test.bat
@@ -1,1 +1,1 @@
-python -m imagedecay.main i o --filterconf=samj.json --loglevel=info --iter=10 --save_steps=3
+python -m imagedecay.main i o --filterconf=samj.json --loglevel=info --iter=10 --save_steps=3  --image_screen_ratio=0.25
