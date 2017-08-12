dea11af979aede5ea5011f8473e4fe18e4276385
Christian Winger <c@wingechr.de>
2017-08-12 07:36:37 +0200

    publish intermmediate

---
+++ example/test.sh
@@ -0,0 +1,1 @@
+python3 -m imagedecay.main i o --filterconf=samj.json --loglevel=debug --iter=10 --save_steps=3  --image_screen_ratio=0.9
--- src/imagedecay/converter.py
+++ src/imagedecay/converter.py
@@ -14,7 +14,7 @@ class Converter(Thread):
     """
     Scan a directory periodically for new files matching a pattern and call a given function
     """
-    def __init__(self, queue_in, queue_out, path, conf, n_iter, save_steps, max_image_size):
+    def __init__(self, queue_in, queue_out, path, conf, n_iter, save_steps, max_image_size, output_fmt='bmp', publish_steps=True):
         super().__init__(target=self.run, daemon=False)
         self.queue_in = queue_in
         self.queue_out = queue_out
@@ -24,6 +24,8 @@ class Converter(Thread):
         self.save_steps = save_steps
         self.running = False
         self.max_image_size = max_image_size
+        self.output_fmt = output_fmt
+        self.publish_steps = publish_steps
 
     def resize(self, img):
         sx = self.max_image_size[0] / img.shape[1]
@@ -53,6 +55,8 @@ class Converter(Thread):
             # save original
             filepath_out = self.get_output_filename(filepath, 0)
             write(im_array, filepath_out)
+            if self.publish_steps:
+                self.queue_out.put([filepath_out])
             imagelist.append(filepath_out)
             for i in range(1, self.n_iter + 1):
                 logging.debug('STEP % 5d' % i)
@@ -62,7 +66,11 @@ class Converter(Thread):
                 if i == self.n_iter or (self.save_steps and i % self.save_steps == 0):
                     filepath_out = self.get_output_filename(filepath, i)
                     write(im_array, filepath_out)
+                    # publish right away
+                    if self.publish_steps:
+                        self.queue_out.put([filepath_out])
                     imagelist.append(filepath_out)
+
             if imagelist:
                 imagelist = tuple(imagelist)
                 logging.debug('CONV queueing %d' % len(imagelist))
@@ -82,7 +90,7 @@ class Converter(Thread):
 
     def get_output_filename(self, filepath, i):
         filename = os.path.basename(filepath)
-        path = os.path.join(self.path, '%s.%06d.png' % (filename, i))
+        path = os.path.join(self.path, '%s.%06d.%s' % (filename, i, self.output_fmt))
         return path
 
 
